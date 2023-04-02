b5a49110f902b0acca8906bcb0f63249d50b1516
Shaurya <72348147+shauryagoyall@users.noreply.github.com>
2023-04-02 19:18:25 +0530

    Add files via upload

--- place_field_estimation.m
+++ place_field_estimation.m
@@ -4,7 +4,6 @@ load('decoded_replay.mat')
 load('extracted_place_fields_BAYESIAN.mat')
 good_place_cells=place_fields_BAYESIAN.good_place_cells;
 
-
 for track_id = 1:2
     relevant_decoded = out(track_id,:);
     relevant_decoded = relevant_decoded(good_place_cells);
@@ -14,6 +13,7 @@ for track_id = 1:2
         cell_id = good_place_cells(i);
         
         spikes_in_position_bin = zeros(1,length(position_bins));
+        spikes_in_position_bin_test = zeros(1,length(position_bins));
         time_in_position_bin = zeros(1,length(position_bins));
         for j = 1:length(decoded_replay)
             left_spike_times = decoded_replay(j).spike_times;
@@ -28,62 +28,64 @@ for track_id = 1:2
                             ind = find(position_bins==decoded_replay(j).estimated_position(l));
                             spikes_in_position_bin(ind) = spikes_in_position_bin(ind) + 1 ;
                        end
+                       
+%%%%%%%%%%% to check if the no spike time bins are being skipped                       
+%                        ind = find(position_bins==decoded_replay(j).estimated_position(l));
+%                        spikes_in_position_bin_test(ind) = spikes_in_position_bin_test(ind) + 1 ;
+
                     end
                 end
             end
         
-        
-%         %linkaxes(ax,'x');
-%         spikes_in_position_bin = zeros(1, length(position_bins));
-%         spike_index = find(spikes_in_time_bin ~=0);
-%         for i=1:length(spike_index)
-%             position_index = find( estimated_position(spike_index(i)) == position_bins     );
-%             spikes_in_position_bin(position_index) = spikes_in_position_bin(position_index) + 1;
-%         end
 
             %% time in position bin
             for k = 1:length(decoded_replay(j).estimated_position)
                 position_index = find(position_bins==decoded_replay(j).estimated_position(k));
                 time_in_position_bin(position_index) = time_in_position_bin(position_index) + 1;
             end
+            
         end
-
+        
+%%%%%%%%%%% to check if the no spike time bins are being skipped         
+%         if ~isequal(spikes_in_position_bin,spikes_in_position_bin_test)
+%             fprintf("track id %.2f cell id  %.2f \n", track_id, cell_id);
+%         end
+        
         %% Place Field
         for i=1:length(position_bins)
             frequency(i) = 10*spikes_in_position_bin(i)/time_in_position_bin(i); %10 as time interval is 0.1s
         end
-        
-        %% Plots
-        figure;
-        subplot(3,1,1);
-        bar(position_bins,spikes_in_position_bin)
-        xticks(position_bins)
-        xlabel("Estimated Position")
-        ylabel("Number of Spikes")
-        title("Number of spikes in a position bin")
-
-        subplot(3,1,2);
-        bar(position_bins,time_in_position_bin)
-        xticks(position_bins)
-        xlabel("Estimated Position")
-        ylabel("Time spent")
-        title("Time in a position bin")
-        
-        subplot(3,1,3);
-        hold on;
-        plot(position_bins,frequency);
-        %if max(decoded_replay(1).place_fields{1, cell_id}{1, 1}) ~= 0
-        actual_place = rescale(decoded_replay(1).place_fields{1, cell_id}{1, 1},0,max(frequency) );
-        plot(position_bins, actual_place ,'r' ); 
-       % end
-        xticks(position_bins);
-        xlabel("Estimated Position");
-        ylabel("Firing Frequency");
-        title("Estimated Place field");
-        hold off;
-        
+         
+%         %% Plots
+%         figure;
+%         subplot(3,1,1);
+%         bar(position_bins,spikes_in_position_bin)
+%         xticks(position_bins)
+%         xlabel("Estimated Position")
+%         ylabel("Number of Spikes")
+%         title("Number of spikes in a position bin")
+% 
+%         subplot(3,1,2);
+%         bar(position_bins,time_in_position_bin)
+%         xticks(position_bins)
+%         xlabel("Estimated Position")
+%         ylabel("Time spent")
+%         title("Time in a position bin")
+%         
+%         subplot(3,1,3);
+%         hold on;
+%         plot(position_bins,frequency);
+%         %if max(decoded_replay(1).place_fields{1, cell_id}{1, 1}) ~= 0
+%         actual_place = rescale(decoded_replay(1).place_fields{1, cell_id}{1, 1},0,max(frequency) );
+%         plot(position_bins, actual_place ,'r' ); 
+%        % end
+%         xticks(position_bins);
+%         xlabel("Estimated Position");
+%         ylabel("Firing Frequency");
+%         title("Estimated Place field");
+%         hold off;
+         
     end
     
-%% Calculating the place field again
 end
 
